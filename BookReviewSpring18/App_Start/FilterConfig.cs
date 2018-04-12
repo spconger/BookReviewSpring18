30272623c2b80ec6dc8a2154c2c624f7f08d146c
Steve <steven.conger@seattlecolleges.edu>
2018-04-12 12:03:45 -0700

    Adoentity commit

---
+++ BookReviewSpring18/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BookReviewSpring18
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
